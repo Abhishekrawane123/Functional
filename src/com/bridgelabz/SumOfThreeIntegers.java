f6ee096826123d4fa1d4d9925f53e28d73ac0419
pknowledge <abhishekrawane1195@gmail.com>
2021-11-02 23:02:21 +0530

    Sum of Three Integers

---
+++ src/com/bridgelabz/SumOfThreeIntegers.java
@@ -0,0 +1,51 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class SumOfThreeIntegers
+{
+public static int findDistinctTriplets(int b[],int length)
+{
+          System.out.println();
+int i,j,k,count=0;
+for(i=0;i<length;i++)
+{
+for(j=i+1;j<length;j++)
+{
+for(k=j+1;k<length;k++)
+{
+
+if (b[i] + b[j] + b[k] == 0)
+{
+System.out.println(b[i] + " " + b[j] + " " + b[k]);
+count++;
+}
+}
+}
+}
+
+return count;
+}
+
+public static void main(String[] args)
+{
+Scanner u1 = new Scanner(System.in);
+System.out.println();
+
+
+System.out.print("Enter Size of Array :");
+int size=u1.nextInt();
+int arr[]=new int [size];
+
+for(int i=0;i<arr.length;i++)
+{
+System.out.print("Enter "+(i+1)+" Value :");
+arr[i]=u1.nextInt();
+}
+
+SumOfThreeIntegers t1 = new SumOfThreeIntegers();
+            int sum=t1.findDistinctTriplets(arr,size);
+              System.out.println("No.of Tripltes :"+sum);
+              System.out.println();
+}
+}
